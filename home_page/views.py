08704d124a056610f763edaebdc2f67f76fbed83
Alexey Mochalov <AlexeyM01@yandex.ru>
2024-03-06 00:14:23 +0300

    creating classes ShowTeacher, HomePageView

--- home_page/views.py
+++ home_page/views.py
@@ -1,50 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
-
 from .forms import *
 from .models import *
+from .utils import *
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-main_menu = [
-    {'title': 'KOREANA', 'url_name': 'home'},
-    {'title': 'Преподаватели', 'url_name': 'teachers'},
-    {'title': 'Перевод', 'url_name': 'translation'},
-    {'title': 'Курсы', 'url_name': 'curses'},
-    #    {'title': 'Отзывы', 'url_name': ''},
-    {'title': 'О нас', 'url_name': 'about_us'},
-    {'title': 'Добавить преподавателя', 'url_name': 'add_teacher'}
-]
-
-
-class show_teachers(ListView):
+class show_teachers(DataMixin, ListView):
     """Класс страницы показа всех преподавателей на базе шаблона teachers.html"""
-    teachers_list = Teacher.objects.all()
     model = Teacher
     template_name = 'home_page/teachers.html'
-    extra_context = {
-        'teachers_list': teachers_list,
-    }
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        """Функция добавления дополнительных контексных данных"""
         context = super().get_context_data(**kwargs)
-        context['main_menu'] = main_menu
-        context['title'] = 'СТРАНИЦА О ПРЕПОДАХ'
-        context['sort'] = self.request.GET.get('sort')
-        return context
+        c_def = self.get_user_context(title="Преподаватели")
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         """Функция возврата списка преподавателей"""
         return Teacher
 
+class HomePageView(DataMixin, ListView):
+    template_name = 'home_page/homepage.html'
 
-def home_page_view(request):
-    """Функция отображения главной страницы с помощью шаблона homepage.html"""
-    context = {
-        'main_menu': main_menu,
-        'title': 'Главная страница'
-    }
-    return render(request, 'home_page/homepage.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="KOREANA")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        """Функция возврата списка преподавателей"""
+        return Teacher
 
 
 def temp_fun(request):
@@ -52,32 +39,27 @@ def temp_fun(request):
     return render(request, 'home_page/somepage.html', {'title': 'Побочная страница'})
 
 
-def show_teacher(request, teacher_slug):
-    """Функция отображения отдельного преподавателя на базе шаблона teacher_page.html"""
-    teacher_info = get_object_or_404(Teacher, slug=teacher_slug)
-    context = {
-        'info': teacher_info,
-        'main_menu': main_menu,
-        'title': 'ПЕРСОНАЛЬНАЯ СТРАНИЦА ПРЕПОДАВАТЕЛЯ',
-    }
-    return render(request, 'home_page/teacher_page.html', context=context)
+class ShowTeacher(DataMixin, DetailView):
+    model = Teacher
+    template_name = 'home_page/teacher_page.html'
+    slug_url_kwarg = 'teacher_slug'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Страница преподавателя')
+        return dict(list(context.items()) + list(c_def.items()))
 
-def add_teacher(request):
-    """Функция добавления преподавателя в БД на базе формы AddTeacherForm и шаблона add_teacher.html"""
-    if request.method == 'POST':
-        form = AddTeacherForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddTeacherForm()
-    context = {
-        'form': form,
-        'main_menu': main_menu,
-        'title': 'Страница добавления учителя'
-    }
-    return render(request, 'home_page/add_teacher.html', context=context)
+class AddTeacher(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddTeacherForm
+    template_name = 'home_page/add_teacher.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Добавление преподавателя")
+        return dict(list(context.items()) + list(c_def.items()))
+
 
 
 def account(request):
